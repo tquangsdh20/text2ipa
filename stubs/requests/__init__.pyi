e358099307e6050ba5f0cc2e72678a6ad1b00fd2
Joseph Quang <tquangsdh20@hcmut.edu.vn>
2021-11-20 11:46:37 +0000

    changes dev.txt

--- stubs/requests/__init__.pyi
+++ stubs/requests/__init__.pyi
@@ -3,4 +3,6 @@ from .exceptions import ConnectTimeout, ReadTimeout, ConnectionError
 
 def get(self, url: str = "", proxies=None, verify=True, timeout=None): ...
 
-__all__ = ["get"]
+def post(self, url: str = "", proxies=None, data=None , timeout=None, verify=True): ...
+
+__all__ = ["get","post"]
